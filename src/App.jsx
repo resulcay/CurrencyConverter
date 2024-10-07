c1f10d6e5605535915f3adb853d4e68bcd596307
resulcay <resulcayop@gmail.com>
2024-10-07 17:10:28 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Currency from './Currency'
+
+function App() {
+  return (
+    <>
+      <Currency />
+    </>
+  )
+}
+
+export default App
---
+++ src/Currency.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import { currencyCodes } from './Data'
+import './Currency.css'
+import axios from 'axios';
+
+export default function Currency() {
+    const currentCurrency = currencyCodes[0];
+    const [baseCurrency, setBaseCurrency] = useState(currentCurrency);
+    const [targetCurrency, setTargetCurrency] = useState(currentCurrency);
+    const [amount, setAmount] = useState(0);
+    const baseUrl = 'https://v6.exchangerate-api.com/v6/';
+    const apiKey = 'your-api-key';
+    let resultObject = document.getElementById('result');
+
+    const handleBaseCurrencyChange = (event) => {
+        setBaseCurrency(event.target.value);
+    }
+
+    const handleTargetCurrencyChange = (event) => {
+        setTargetCurrency(event.target.value);
+    }
+
+    const handleAmountChange = (event) => {
+        setAmount(event.target.value);
+    }
+
+    const convert = async () => {
+        // https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP
+        const url = `${baseUrl}${apiKey}/pair/${baseCurrency}/${targetCurrency}`;
+        try {
+            const response = await axios.get(url);
+            const rate = response.data.conversion_rate;
+            const result = amount * rate;
+            const conversion_result = response.data.conversion_result;
+            resultObject.value = result;
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+    return (
+        <div className='currency-main'>
+            <div className='currency-base'>
+                <label htmlFor="baseCurrency">Base Currency</label>
+                <select className='select' name="baseCurrency" onChange={handleBaseCurrencyChange}>
+                    {
+                        currencyCodes.map((code, index) => {
+                            return <option key={index} value={code}>{code}</option>
+                        })
+                    }
+                </select>
+                <input type="number" value={amount} placeholder="Amount" min={0} onChange={handleAmountChange} />
+            </div>
+            <div className='buttons'>
+                <button className='convert-button' onClick={() => convert()}>Convert</button>
+                <div></div>
+                <button className='clear-button' onClick={() => {
+                    setAmount(0);
+                    resultObject.value = 0;
+                }}>Clear</button>
+            </div>
+            <div className='currency-base'>
+                <label htmlFor='targetCurrency'>Target Currency</label>
+                <select className='select' name="targetCurrency" onChange={handleTargetCurrencyChange}>
+                    {
+                        currencyCodes.map((code, index) => {
+                            return <option key={index} value={code}>{code}</option>
+                        })
+                    }
+                </select>
+                <input type="number" id="result" readOnly disabled />
+            </div>
+        </div>
+    )
+}
